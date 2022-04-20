cadd2c0199dd16abbee8c87e651e464d2756f3cb
jamiewood678 <jamie.woods@devtropolis.co.uk>
2022-04-20 17:07:27 +0100

    final

--- dist/index.js
+++ dist/index.js
@@ -8507,7 +8507,6 @@ async function run() {
     console.error(error);
   }
   labelString = labels.data.reduce((prev, label, index) => prev + label.name + (index !== labels.length - 1 ? ', ' : ''), labelString);
-  console.log(labelString);
 
   try {
     const createReleaseResponse = await github.rest.repos.createRelease({
@@ -8515,7 +8514,7 @@ async function run() {
       repo,
       tag_name: getNewVersion(latestReleaseTag),
       name: title,
-      body: body ? body + '\r\n' + labelString : "",
+      body: body ? body + '\r\n' + labelString : "" + '\r\n' + labelString,
     });
     if(createReleaseResponse.status !== 201) {
       console.error("An error occurred while creating release");
